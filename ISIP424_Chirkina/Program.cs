b0a2d9a9cdcaf1d371a5bbc2b64c016b30a7d5a7
243587 <243587@KB37-118-C02>
2026-09-11 14:57:06 +0300

    1

--- ISIP424_Chirkina/Program.cs
+++ ISIP424_Chirkina/Program.cs
@@ -10,7 +10,58 @@ namespace ISIP424_Chirkina
     {
         static void Main(string[] args)
         {
-          
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            // выбор
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine();
+                    Console.WriteLine("Все расходы: ");
+                    break;
+
+                case 3:
+                    Console.WriteLine();
+                    
+                    break;
+
+                case 4:
+                    Console.WriteLine();
+                    Console.WriteLine("Доступные валюты:");
+
+                    break;
+
+                case 5:
+                    Console.WriteLine();
+                    Console.Write("Введите название: ");
+                    string search = Console.ReadLine();
+
+                    bool found = false;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (names[i] == search)
+                        {
+                            Console.WriteLine(names[i] + " - " + prices[i] + " руб.");
+                            found = true;
+                        }
+                    }
+
+                    if (found == false)
+                    {
+                        Console.WriteLine("Ничего не найдено.");
+                    }
+
+                    break;
+
+                case 0:
+                    Console.WriteLine("Программа завершена, Босс");
+                    break;
+
+                default:
+                    Console.WriteLine("Такого выбора нет :( ");
+                    break;
+            }
         }
     }
 }
