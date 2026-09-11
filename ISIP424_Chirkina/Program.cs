0b35e1661af03afb7ffccbbf8c6e59f18a86f5c9
243587 <243587@KB37-118-C02>
2026-09-11 15:07:32 +0300

    2

--- ISIP424_Chirkina/Program.cs
+++ ISIP424_Chirkina/Program.cs
@@ -10,58 +10,93 @@ namespace ISIP424_Chirkina
     {
         static void Main(string[] args)
         {
-            choice = Convert.ToInt32(Console.ReadLine());
-
-            // выбор
-            switch (choice)
+            int choice = -1;
+            while (choice != 0)
             {
-                case 1:
-                    Console.WriteLine();
-                    Console.WriteLine("Все расходы: ");
-                    break;
+                Console.WriteLine();
+                Console.WriteLine("     МЕНЮ:   ");
+                Console.WriteLine("1. Добавить товар");
+                Console.WriteLine("2. Удалить товар");
+                Console.WriteLine("3. Заказать поставку товара");
+                Console.WriteLine("4. Продать товар");
+                Console.WriteLine("5. Поиск товаров (по коду, названию и категории)");
+                Console.WriteLine("0. Выход");
+                Console.WriteLine();
+                Console.WriteLine("Выберете пункт: ");
+                choice = Convert.ToInt32(Console.ReadLine());
 
-                case 3:
-                    Console.WriteLine();
-                    
-                    break;
+                choice = Convert.ToInt32(Console.ReadLine());
 
-                case 4:
-                    Console.WriteLine();
-                    Console.WriteLine("Доступные валюты:");
+                // выбор
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        Console.WriteLine("Введите товар который хотите добавить: ");
 
-                    break;
+                        break;
 
-                case 5:
-                    Console.WriteLine();
-                    Console.Write("Введите название: ");
-                    string search = Console.ReadLine();
+                    case 2:
+                        Console.WriteLine();
+                        Console.WriteLine("Список товаров на данный момент: ");
+                        Console.WriteLine("Введите товер, который хотите удалить: ");
 
-                    bool found = false;
+                        break;
 
-                    for (int i = 0; i < n; i++)
-                    {
-                        if (names[i] == search)
+                    case 3:
+                        Console.WriteLine();
+                        Console.WriteLine("Введите какой товар хотите заказать: ");
+
+                        break;
+
+                    case 4:
+                        Console.WriteLine();
+                        Console.WriteLine("Введите какой товар хотите продать: ");
+
+                        break;
+
+                    case 5:
+                        Console.WriteLine();
+                        Console.Write("Введите код, название или категорию: ");
+                        string search = Console.ReadLine();
+
+                        bool found = false;
+
+                        for (int i = 0; i < n; i++)
                         {
-                            Console.WriteLine(names[i] + " - " + prices[i] + " руб.");
-                            found = true;
+                            if (names[i] == search)
+                            {
+                                Console.WriteLine(names[i] + " - " + prices[i] + " руб.");
+                                found = true;
+                            }
                         }
-                    }
 
-                    if (found == false)
-                    {
-                        Console.WriteLine("Ничего не найдено.");
-                    }
+                        if (found == false)
+                        {
+                            Console.WriteLine("Ничего не найдено.");
+                        }
 
-                    break;
+                        break;
 
-                case 0:
-                    Console.WriteLine("Программа завершена, Босс");
-                    break;
+                    case 0:
+                        Console.WriteLine("Программа завершена, Босс");
+                        break;
 
-                default:
-                    Console.WriteLine("Такого выбора нет :( ");
-                    break;
+                    default:
+                        Console.WriteLine("Такого выбора нет :( ");
+                        break;
+                }
             }
+        }
+
+        class Thing
+        {
+            public int ID;
+            public string name;
+            public int cost;
+            public int kolvo;
+            public int nalichie;
+            public string category;
         }
     }
 }
